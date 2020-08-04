fab69c115294c0494e604fe0430de514a8ff9530
takeru1205 <takeru0639418@gmail.com>
2020-08-04 22:37:19 +0900

    [update] tau parameter

--- const.py
+++ const.py
@@ -4,3 +4,4 @@ gamma = 0.99  # discount factor
 weight_decay = 10e-2  # L2 weight decay
 actor_lr = 10e-4  # learning rate for actor network
 critic_lr = 10e-3  # learning rate for critic network
+tau = 0.001  # soft target update coefficient
