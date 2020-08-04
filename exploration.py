9e3ecbb3f2b1763e2cabdcd71eeb6c70d0c47602
takeru1205 <takeru0639418@gmail.com>
2020-08-04 23:34:56 +0900

    [update] default value of mu set as 0.0

--- exploration.py
+++ exploration.py
@@ -3,21 +3,20 @@ import numpy as np
 
 
 class OUActionNoise(object):
-    def __init__(self, mu, sigma=0.2, theta=0.15, dt=1e-2, x0=None):
+    def __init__(self, action_dim, mu=0.0, sigma=0.2, theta=0.15, dt=1e-2, x0=None):
         self.mu = mu
         self.sigma = sigma
         self.theta = theta
         self.dt = dt
         self.x0 = x0
         self.x_prev = self.reset()
+        self.action_dim = action_dim
 
     def __call__(self):
         x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
-            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.action_dim)
         self.x_prev = x
         return x
 
     def reset(self):
-        return self.x0 if self.x0 is not None else np.zeros_like(self.mu)
-
-
+        return self.x0 if self.x0 is not None else np.ones(self.action_dim) * self.mu
