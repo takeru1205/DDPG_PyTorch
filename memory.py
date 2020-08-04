bc3bbd7e2a5b615b91d2d14ac61041ac65e74547
takeru1205 <takeru0639418@gmail.com>
2020-08-04 21:03:14 +0900

    [fix] run only on GPU machine

--- memory.py
+++ memory.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class ReplayMemory(object):
-    def __init__(self, state_dim, act_dim, max_size=int(10e+6), cuda=True):
+    def __init__(self, state_dim, act_dim, max_size=int(10e+6)):
         self.state_memory = torch.zeros((self.max_size, *state_dim), dtype=torch.float)
         self.new_state_memory = torch.zeros((self.max_size, *state_dim), dtype=torch.float)
         self.action_memory = torch.zeros((self.max_size, act_dim), dtype=torch.uint8)
@@ -12,7 +12,6 @@ class ReplayMemory(object):
         self.terminal_memory = torch.zeros(self.max_size, dtype=torch.uint8)
         self.max_size = max_size
         self.mem_ctrl = 0
-        self.cuda = cuda
 
     def store_transition(self, state, action, reward, state_, done):
         index = self.mem_ctrl % self.max_size
@@ -33,7 +32,4 @@ class ReplayMemory(object):
         states_ = self.new_state_memory[batch]
         terminal = self.terminal_memory[batch]
 
-        if self.cuda:
-            return states.to('cuda'), actions.to('cuda'), states_.to('cuda'), rewards.to('cuda'), terminal
-
-        return states, states_, actions, rewards, terminal
+        return states.to('cuda'), actions.to('cuda'), states_.to('cuda'), rewards.to('cuda'), terminal
