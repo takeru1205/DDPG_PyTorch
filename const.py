7e6975a9b214da838f4a3bd560c0a31040340a2a
takeru1205 <takeru0639418@gmail.com>
2020-08-04 21:02:19 +0900

    [add] constant parameters file

---
+++ const.py
@@ -0,0 +1,6 @@
+# constant parameters
+
+gamma = 0.99  # discount factor
+weight_decay = 10e-2  # L2 weight decay
+actor_lr = 10e-4  # learning rate for actor network
+critic_lr = 10e-3  # learning rate for critic network
