[fix] upper of formula(6)

agent.py
@@ -77,9 +77,9 @@ class DDPG(object):
         self.critic_optimizer.step()
 
         # Update Actor (Policy Gradient)
-        j = -1 * torch.mean(self.critic(states, actions) * self.actor(states))
+        j = -1 * torch.mean(self.critic(states, self.actor(states)))
         if self.writer:
-            self.writer.add_scalar("loss/actor", j, time_step)
+            self.writer.add_scalar("loss/actor", j.item(), time_step)
         self.actor_optimizer.zero_grad()
         j.backward()
         self.actor_optimizer.step()