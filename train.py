5fe9d744cb270f96a9da2589c0efaf8ad8f00d35
takeru1205 <takeru0639418@gmail.com>
2020-08-05 12:11:49 +0900

    [update] tensorboard

--- train.py
+++ train.py
@@ -16,9 +16,9 @@ env.seed(42)
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
 
-agent = DDPG(env)
-
 writer = SummaryWriter(log_dir='logs/')
+agent = DDPG(env, writer)
+
 all_timesteps = 0
 
 for e in range(epoch):
@@ -28,16 +28,15 @@ for e in range(epoch):
     for timestep in range(200):
         action = agent.get_action(state, noise)
         state_, reward, done, _ = env.step(action)
+        # env.render()
         agent.store_transition(state, action, state_, reward, done)
 
         cumulative_reward += reward
 
-        critic_loss, actor_loss = agent.update()
-        writer.add_scalar("loss/critic", critic_loss, all_timesteps)
-        writer.add_scalar("loss/actor", actor_loss, all_timesteps)
+        agent.update(all_timesteps)
         all_timesteps += 1
     print('Epoch : {} / {}, Cumulative Reward : {}'.format(e, epoch, cumulative_reward))
-    writer.add_scalar("reward", cumulative_reward, epoch)
+    writer.add_scalar("reward", cumulative_reward, e)
 
 agent.save_model()
 
