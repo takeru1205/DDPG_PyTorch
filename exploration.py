[fix] formula

exploration.py
@@ -10,20 +10,28 @@ class OUActionNoise(object):
 
     """
 
-    def __init__(self, action_dim, mu=0.0, sigma=0.2, theta=0.15, dt=1e-2, x0=None):
+    def __init__(self, action_dim, mu=0.0, sigma=0.2,
+                 max_sigma=0.3, min_sigma=-0.3, theta=0.15, decay_period=100000, x0=None):
         self.mu = mu
         self.sigma = sigma
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
         self.theta = theta
-        self.dt = dt
         self.x0 = x0
         self.action_dim = action_dim
-        self.x_prev = self.reset()
+        self.decay_period = decay_period
+        self.reset()
 
-    def __call__(self):
-        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
-            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.action_dim)
-        self.x_prev = x
-        return x
+    def evolve_state(self):
+        x = self.x0
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(self.action_dim)
+        self.x0 = x + dx
+        return self.x0
+
+    def __call__(self, t=0):
+        ou_state = self.evolve_state()
+        self.sigma = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        return ou_state
 
     def reset(self):
-        return self.x0 if self.x0 is not None else np.ones(self.action_dim) * self.mu
+        self.x0 = np.ones(self.action_dim) * self.mu