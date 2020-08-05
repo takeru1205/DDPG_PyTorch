4ab5f5cae128916f2876d28f5d3c9fdd6bf91bdd
takeru1205 <takeru0639418@gmail.com>
2020-08-05 17:55:32 +0900

    [fix] 10e to 1.0e

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 # constant parameters
 
 gamma = 0.99  # discount factor
-weight_decay = 10e-2  # L2 weight decay
-actor_lr = 10e-4  # learning rate for actor network
-critic_lr = 10e-3  # learning rate for critic network
+weight_decay = 1.0e-2  # L2 weight decay
+actor_lr = 1.0e-4  # learning rate for actor network
+critic_lr = 1.0e-3  # learning rate for critic network
 tau = 0.001  # soft target update coefficient
