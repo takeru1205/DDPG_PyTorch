b0539ca220553809fe0e2c729b576e5b3fe6c90d
takeru1205 <takeru0639418@gmail.com>
2020-08-06 17:57:51 +0900

    [add] test

---
+++ test.py
@@ -0,0 +1,19 @@
+import gym
+from agent import DDPG
+
+
+epoch = 500
+env = gym.make('Pendulum-v0')
+
+agent = DDPG(env)
+agent.load_model()
+
+state = env.reset()
+
+cumulative_reward = 0
+for i in range(200):
+    action = agent.get_action(state)
+    env.render()
+    state, reward, _, _ = env.step(action * 2)
+    cumulative_reward += reward
+print('Cumulative Reward: {}'.format(cumulative_reward))
