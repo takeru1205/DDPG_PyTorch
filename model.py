acac6fcd379648aa2f5691437426b6639f33c2eb
takeru1205 <takeru0639418@gmail.com>
2020-08-06 18:00:11 +0900

    [update] hiddensize=16, concat action in first layer

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 
 from utils import layer_init, actor_last_layer_init, critic_last_layer_init
 
+HIDDEN_SIZE = 16
+
 
 class Actor(nn.Module):
     """
@@ -13,9 +15,9 @@ class Actor(nn.Module):
 
     def __init__(self, state_dim, action_dim):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 400)
-        self.fc2 = nn.Linear(400, 300)
-        self.fc3 = nn.Linear(300, action_dim)
+        self.fc1 = nn.Linear(state_dim, HIDDEN_SIZE)
+        self.fc2 = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+        self.fc3 = nn.Linear(HIDDEN_SIZE, action_dim)
 
         # Initialize actor weights
         layer_init(self.fc1)
@@ -36,9 +38,9 @@ class Critic(nn.Module):
 
     def __init__(self, state_dim, action_dim):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 400)
-        self.fc2 = nn.Linear(400, 300)
-        self.fc3 = nn.Linear(300 + action_dim, 1)
+        self.fc1 = nn.Linear(state_dim + action_dim, HIDDEN_SIZE)
+        self.fc2 = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+        self.fc3 = nn.Linear(HIDDEN_SIZE, 1)
 
         # Initialize critic weights
         layer_init(self.fc1)
@@ -46,7 +48,7 @@ class Critic(nn.Module):
         critic_last_layer_init(self.fc3)
 
     def forward(self, x, action):
-        x = F.softplus(self.fc1(x))
+        x = F.softplus(self.fc1(torch.cat([x, action], dim=1)))
         x = F.softplus(self.fc2(x))
-        x = self.fc3(torch.cat([x, action], dim=1))
+        x = self.fc3(x)
         return x
